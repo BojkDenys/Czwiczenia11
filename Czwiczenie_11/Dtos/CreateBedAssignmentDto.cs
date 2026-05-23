e56f973529d9c9982fde3483bd84b05fdeba625b
Denys Boiko <denisbojko2005@gmail.com>
2026-05-23 14:19:34 +0200

    add CreateBedAssignmentDto.cs

---
+++ Czwiczenie_11/Dtos/CreateBedAssignmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Czwiczenie_11.Dtos;
+
+public class CreateBedAssignmentDto
+{
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    public string BedType { get; set; } = string.Empty;
+    public string Ward { get; set; } = string.Empty;
+}
